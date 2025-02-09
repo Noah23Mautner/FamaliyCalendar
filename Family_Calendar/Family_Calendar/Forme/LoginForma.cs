fba7d99bfbe9995e82ce4fbb99bdb7d86c19414f
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2025-02-09 19:17:52 +0100

    Add files via upload

---
+++ Family_Calendar/Family_Calendar/Forme/LoginForma.cs
@@ -0,0 +1,53 @@
+﻿using Family_Calendar.NewFolder1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Family_Calendar
+{
+    public partial class LoginForm : Form
+    {
+        
+
+        public LoginForm()
+        {
+            
+
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            
+
+            if (txtEmail.Text.Trim().ToLower() == "admin" && txtPassword.Text.Trim() == "1234")
+            {
+                MessageBox.Show("Uspešno ste se prijavili!", "Prijava", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                MainForm mainForm = new MainForm();
+                mainForm.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Pogrešan email ili lozinka!", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ Family_Calendar/Family_Calendar/Klase/Korisnik.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Family_Calendar.Klase
+{
+    internal class Korisnik
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Email { get; set; }
+        public string LozinkaHash { get; set; }
+        public int ObiteljId { get; set; } 
+    }
+}
